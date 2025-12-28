ca1150b6aab464cebc3569d6636a41a42ddec75c
NaroX007 <ariqhidayat101@gmail.com>
2025-12-28 18:22:24 +0700

    third commit

--- general.js
+++ general.js
@@ -2,88 +2,62 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-/**
- * TASK 2
- * Get all books using async/await + Axios
- */
+const BASE_URL = "http://localhost:5000/";
+
 router.get("/books", async (req, res) => {
   try {
-    const response = await axios.get("http://localhost:5000/");
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+    const response = await axios.get(BASE_URL);
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
-/**
- * TASK 3
- * Get book by ISBN using async/await + Axios
- */
 router.get("/books/isbn/:isbn", async (req, res) => {
   try {
-    const response = await axios.get("http://localhost:5000/");
-    const books = response.data;
-    const book = books[req.params.isbn];
-
-    if (book) res.status(200).json(book);
+    const response = await axios.get(BASE_URL);
+    const book = response.data[req.params.isbn];
+    if (book) res.json(book);
     else res.status(404).json({ message: "Book not found" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
-/**
- * TASK 4
- * Get books by author using Promise + Axios
- */
-router.get("/books/author/:author", (req, res) => {
-  const author = req.params.author.toLowerCase();
+router.get("/books/author/:author", async (req, res) => {
+  try {
+    const response = await axios.get(BASE_URL);
+    const books = response.data;
+    const result = [];
 
-  axios
-    .get("http://localhost:5000/")
-    .then((response) => {
-      const books = response.data;
-      const result = [];
+    for (let key in books) {
+      if (books[key].author.toLowerCase() === req.params.author.toLowerCase()) {
+        result.push({ isbn: key, ...books[key] });
+      }
+    }
 
-      Object.keys(books).forEach((isbn) => {
-        if (books[isbn].author.toLowerCase() === author) {
-          result.push({ isbn, title: books[isbn].title });
-        }
-      });
-
-      if (result.length > 0) res.status(200).json(result);
-      else res.status(404).json({ message: "No books found" });
-    })
-    .catch((error) => {
-      res.status(500).json({ message: error.message });
-    });
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-/**
- * TASK 5
- * Get books by title using Promise + Axios
- */
-router.get("/books/title/:title", (req, res) => {
-  const title = req.params.title.toLowerCase();
+router.get("/books/title/:title", async (req, res) => {
+  try {
+    const response = await axios.get(BASE_URL);
+    const books = response.data;
+    const result = [];
 
-  axios
-    .get("http://localhost:5000/")
-    .then((response) => {
-      const books = response.data;
-      const result = [];
+    for (let key in books) {
+      if (books[key].title.toLowerCase() === req.params.title.toLowerCase()) {
+        result.push({ isbn: key, ...books[key] });
+      }
+    }
 
-      Object.keys(books).forEach((isbn) => {
-        if (books[isbn].title.toLowerCase() === title) {
-          result.push({ isbn, author: books[isbn].author });
-        }
-      });
-
-      if (result.length > 0) res.status(200).json(result);
-      else res.status(404).json({ message: "No books found" });
-    })
-    .catch((error) => {
-      res.status(500).json({ message: error.message });
-    });
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
