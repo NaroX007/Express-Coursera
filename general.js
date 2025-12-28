2ef91820471b473284b5abdf0b9611c865cb50b8
NaroX007 <ariqhidayat101@gmail.com>
2025-12-28 18:08:24 +0700

    Fix Task 11 using axios async/await

--- general.js
+++ general.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
-let books = require("./booksdb.js");
 
 /**
  * TASK 2
- * Get all books (ASYNC)
+ * Get all books using async/await + Axios
  */
 router.get("/books", async (req, res) => {
   try {
-    res.status(200).json(books);
+    const response = await axios.get("http://localhost:5000/");
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -16,18 +17,16 @@ router.get("/books", async (req, res) => {
 
 /**
  * TASK 3
- * Get book by ISBN (ASYNC)
+ * Get book by ISBN using async/await + Axios
  */
 router.get("/books/isbn/:isbn", async (req, res) => {
   try {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
+    const response = await axios.get("http://localhost:5000/");
+    const books = response.data;
+    const book = books[req.params.isbn];
 
-    if (book) {
-      res.status(200).json(book);
-    } else {
-      res.status(404).json({ message: "Book not found" });
-    }
+    if (book) res.status(200).json(book);
+    else res.status(404).json({ message: "Book not found" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -35,67 +34,56 @@ router.get("/books/isbn/:isbn", async (req, res) => {
 
 /**
  * TASK 4
- * Get books by Author (PROMISE)
+ * Get books by author using Promise + Axios
  */
 router.get("/books/author/:author", (req, res) => {
   const author = req.params.author.toLowerCase();
 
-  new Promise((resolve, reject) => {
-    const result = [];
+  axios
+    .get("http://localhost:5000/")
+    .then((response) => {
+      const books = response.data;
+      const result = [];
 
-    Object.keys(books).forEach((isbn) => {
-      if (books[isbn].author.toLowerCase() === author) {
-        result.push({ isbn, title: books[isbn].title });
-      }
+      Object.keys(books).forEach((isbn) => {
+        if (books[isbn].author.toLowerCase() === author) {
+          result.push({ isbn, title: books[isbn].title });
+        }
+      });
+
+      if (result.length > 0) res.status(200).json(result);
+      else res.status(404).json({ message: "No books found" });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
     });
-
-    if (result.length > 0) resolve(result);
-    else reject("No books found for this author");
-  })
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
 });
 
 /**
  * TASK 5
- * Get books by Title (PROMISE)
+ * Get books by title using Promise + Axios
  */
 router.get("/books/title/:title", (req, res) => {
   const title = req.params.title.toLowerCase();
 
-  new Promise((resolve, reject) => {
-    const result = [];
+  axios
+    .get("http://localhost:5000/")
+    .then((response) => {
+      const books = response.data;
+      const result = [];
 
-    Object.keys(books).forEach((isbn) => {
-      if (books[isbn].title.toLowerCase() === title) {
-        result.push({ isbn, author: books[isbn].author });
-      }
+      Object.keys(books).forEach((isbn) => {
+        if (books[isbn].title.toLowerCase() === title) {
+          result.push({ isbn, author: books[isbn].author });
+        }
+      });
+
+      if (result.length > 0) res.status(200).json(result);
+      else res.status(404).json({ message: "No books found" });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
     });
-
-    if (result.length > 0) resolve(result);
-    else reject("No books found for this title");
-  })
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
-});
-
-/**
- * TASK 6
- * Get book review
- */
-router.get("/books/review/:isbn", async (req, res) => {
-  try {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-
-    if (book) {
-      res.status(200).json(book.reviews);
-    } else {
-      res.status(404).json({ message: "Book not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 module.exports = router;
